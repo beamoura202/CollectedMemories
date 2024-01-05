b61334da19af3200447a612a73e783330995b7ab
Beatriz Moura <beatrizmoura.slb@gmail.com>
2024-01-05 22:16:09 +0000

    a

--- js.js
+++ js.js
@@ -19,7 +19,7 @@ window.addEventListener("DOMContentLoaded", function () {
     }
     
     // Exemplo de uso:
-    const urlDaAPI = "https://exemplo.com/api/dados";
+    const urlDaAPI = "https://api.cosmicjs.com/v3/buckets/collected-memories-production-19d268e0-ab2a-11ee-ba66-8b61b87e3752/objects/65987df4723ffd2d238b5d07?read_key=KyYPncCMqJ14IQonFQdyh5yIKfZGRRDHqg93DHO0coRKHy1iLw&depth=1&props=slug,title,metadata,";
     const parametrosDaAPI = { chave: "valor" };
     
     buscarDadosDaAPI(urlDaAPI, parametrosDaAPI)
